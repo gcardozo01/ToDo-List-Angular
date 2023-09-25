e90bb0a4762080a0244cc520b7a38d18b44130b6
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-09-25 17:39:23 -0300

    Initial commit

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+// Components
+import { HeaderComponent } from './components/header/header.component';
+import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { HomeComponent } from './pages/home/home.component';
+
+// Pages
+import { TodoInputAddItensComponent } from './components/todo-input-add-itens/todo-input-add-itens.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    TodoButtonDeleteAllComponent,
+    TodoListComponent,
+    HomeComponent,
+    TodoInputAddItensComponent,
+  ],
+  imports: [CommonModule, FormsModule],
+})
+export class HomeModule {}
